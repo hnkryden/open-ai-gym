f9e92e181a18f948bc6e683652aea2aab148bace
henrikryden <henrik.g.ryden@hotmail.com>
2019-03-09 12:28:41 +0100

    mcar

--- mountainCar.py
+++ mountainCar.py
@@ -8,103 +8,68 @@ from keras.layers import Dense
 import gym
 import numpy as np
 from collections import deque
+from dqn import DQN
+from dqn_scorer import dqnScorerMountainCar
 
 import random
 
-# From https://swaathi.com/2017/04/29/normalizing-data/
-def normalizeState(obs,_env):
-    l = _env.observation_space.low
-    h = _env.observation_space.high
-    obs_norm = (obs - l) / (h-l)
-    return obs_norm
+plt.figure()
+plt.plot(list(range(10)))
 
-def normalizeAction(action):
-    return (action -0)/(2-0)
 
-def getMaxQvalues(obs,_model,_env):
-    obsNorm = normalizeState(obs,_env)
-    allActions = np.array([np.append(obsNorm, normalizeAction(0)),
-                           np.append(obsNorm, normalizeAction(1)),
-                           np.append(obsNorm, normalizeAction(2))])
-    q_values = _model.predict(allActions)
-    #print(q_values)
-    return q_values
-
-def main():
+def main(lr=0.001):
+    np.random.seed(0)
     env = gym.make('MountainCar-v0')
-
-    env.reset()
     model = keras.Sequential()
-    model.add(Dense(24, activation= "relu",input_dim=3, kernel_initializer='normal'))
-    model.add(Dense(24, activation= "relu"))
-    #model.add(Dense(3, activation= "relu"))
-    model.add(Dense(1, kernel_initializer='normal',activation="linear"))
+    model.add(Dense(128, activation= "relu",input_dim=3, kernel_initializer='normal'))
+    model.add(Dense(52, activation= "relu"))
+    model.add(Dense(1, kernel_initializer='normal', activation="linear"))
+    adam = keras.optimizers.Adam(lr=lr)
+    model.compile(loss='mean_squared_error', optimizer=adam)
 
-
-    adam = keras.optimizers.Adam()#lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
     gamma = 0.95
+    memorySize = 200*100
+    dqn = DQN(model, gamma, memorySize,replaysize=64,_env = env)
+    dqnScore = dqnScorerMountainCar(dqn,_env=env)
+    nrofEpisodes = 1000
 
-    model.compile(loss='mean_squared_error', optimizer= adam)
-
-    nrofEpisodes = 5000
-    epsilon = 1
-    memorySize = 5000
-    res = np.zeros(shape=(nrofEpisodes,2))
-
-    memories = deque(maxlen=memorySize) #Experience replay
-    #actualArr = deque(maxlen=memorySize)
+    res = np.zeros(shape=(nrofEpisodes, 2))
 
     for episode in range(nrofEpisodes):
-
-        #env.render()
-
-        if (epsilon > 0.1):
-            epsilon *= 0.995
-
-        obs, _, done, _ = env.step(env.action_space.sample())
-        if (episode % 100) == 0:
-            print("episode " , episode, epsilon,len(memories))
-            print(res[episode-1,:])
-            print("--- %s seconds ---" % (time.time() - start_time))
-
-            #epsilon -= 0.2
-        while not done:
-
-            if np.random.rand() < epsilon:
-                action = 0 if np.random.rand() > 0.5 else 2
-            else:
-                q_values = getMaxQvalues(obs, model,env)
-                action = np.argmax(q_values)
-
-            new_obs, reward, done, info = env.step(action)
-
-            predicted = np.append(normalizeState(obs,env), normalizeAction(action))
-            actual = reward + gamma * np.max(getMaxQvalues(new_obs,model,env))
-            memories.append(np.append(predicted,actual))
-            #actualArr.append(actual)
-            obs = new_obs
-
-        replay = np.vstack(random.sample(memories, 199))
-
-        x = replay[:,0:3]
-        y = replay[:,3]
-        #trainIdx = np.random.choice(len(y), 128, replace = False)
-
-        #if(len(y)==200):
-        #    y[-1] = -200
-        #print("%.2f min , %.2f max " % (np.min(x[:,0]),np.max(x[:,0])))
-        history = model.fit(x, y, epochs=1, verbose=0, batch_size=32)
-        #print(history.history,np.min(y),np.max(y))
         env.reset()
-        res[episode,:] = [np.min(x[:,0]),np.max(x[:,0])]
+        action = 0
+        obs, _, done, _ = env.step(action)
+        #if (episode % 100) == 10:
+        if (episode % 100) == 10:
+            print("episode " , episode)
+            dqnScore.printDistance()
+            #dqnScore.plot_cost_to_ßgo_mountain_car()
+            #print(res[episode-1,:])
+            print("--- %s seconds ---" % (time.time() - start_time))
+        iter = 0
+        while not done:
+            iter += 1
+            action = dqn.action(obs)
+            new_obs, reward, done, info = env.step(action)
+            dqn.add(action,obs,new_obs,reward)
+            obs = new_obs
+            if(done and (iter<199)):
+                print("****Success*****")
+            #if(episode % 100) == 10:
+            #    env.render()
+        dqn.replay()
+        env.reset()
+        dqnScore.updateResult()
+        #res[episode,:] = [np.min(x[:,0]),np.max(x[:,0])]
 
-    plt.plot(res)
+    dqnScore.plotResults()
+    dqnScore.plot_cost_to_go_mountain_car()
     plt.show()
 
 
+#lr =[0.1,0.01,0.001,0.0001]
 
-
-main()
+main(lr=0.004)
 
 
 
